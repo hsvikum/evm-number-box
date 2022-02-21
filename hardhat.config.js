02374d470b701e3e41f1652fb7d7727a293570f6
Shalitha Vikum <hsvikum@gmail.com>
2022-02-21 12:54:12 +0530

    setting ropsten network configuration for hardhat ⚙️

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 require("@nomiclabs/hardhat-waffle");
 
 // This is a sample Hardhat task. To learn how to create your own go to
@@ -20,5 +21,11 @@ module.exports = {
   solidity: "0.8.4",
   paths: {
     artifacts: "./frontend/artifacts"
+  },
+  networks: {
+    ropsten: {
+      url: `${process.env.ROPSTEN_URL}`,
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    }
   }
 };
